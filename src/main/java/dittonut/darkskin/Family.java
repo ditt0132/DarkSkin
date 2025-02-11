4b8f566c37f256f3fe4eafe75026a0c2564f87d9
plz-Enter-name <104212145+plz-Enter-name@users.noreply.github.com>
2025-02-12 00:07:29 +0900

    Family -> FamilyUtil. 새 클래스: Family (Record): 가문을 표현함. 쓰일지는 모르겠지만 일단 만듬

--- src/main/java/dittonut/darkskin/Family.java
+++ src/main/java/dittonut/darkskin/Family.java
@@ -1,20 +1,10 @@
 package dittonut.darkskin;
 
+import net.kyori.adventure.text.Component;
+
+import java.util.Collection;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Team;
-
-public class Family {
-  public static Team getByOwner(String ownerName) {
-    return Bukkit.getScoreboardManager().getMainScoreboard().getTeam("dt."+ownerName);
-  }
-  
-  public static Team getTeam(UUID id) {
-    return Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(Bukkit.getOfflinePlayer(id));
-  }
-  public static Team getTeam(Player p) {
-    return getTeam(p.getUniqueId());
-  }
+public record Family(UUID ownerId, Collection<UUID> players, Component name) {
+    
 }
---
+++ src/main/java/dittonut/darkskin/FamilyUtil.java
@@ -0,0 +1,20 @@
+package dittonut.darkskin;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
+
+public class FamilyUtil {
+  public static Team getByOwner(String ownerName) {
+    return Bukkit.getScoreboardManager().getMainScoreboard().getTeam("dt."+ownerName);
+  }
+  
+  public static Team getTeam(UUID id) {
+    return Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(Bukkit.getOfflinePlayer(id));
+  }
+  public static Team getTeam(Player p) {
+    return getTeam(p.getUniqueId());
+  }
+}
